199f9011aaf84699f1d8f5f3a32ab9265fa136a8
Anthony Dotterer <anthony.dotterer@gmail.com>
2019-05-03 20:43:21 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# this is an official Python runtime, used as the parent image
+FROM python:3.7-slim
+
+# set the working directory in the container to /app
+WORKDIR /app
+
+# add the current directory to the container as /app
+ADD ./requirements.txt /app/requirements.txt
+
+# execute everyone's favorite pip command, pip install -r
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# add the current directory to the container as /app
+ADD . /app
+
+# unblock port 80 for the Flask app to run on
+EXPOSE 5000
+
+# execute the Flask app
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,103 @@
+
+from flask import Flask, request
+
+from threading import Timer
+
+from datetime import datetime, timezone
+import dateutil.parser
+
+import logging
+
+from apns2.client import APNsClient
+from apns2.payload import Payload
+from apns2.credentials import TokenCredentials
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
+
+pusher = None
+app = Flask("ChillzieServer")
+
+
+@app.route("/alarm", methods=['POST', 'GET'])
+def alarm():
+    error = None
+    if request.method == 'POST':
+        token_hex = request.form["token_hex"]
+        alarm_str = request.form["alarm"]
+
+        logging.debug("Received request: TOKEN=%s, ALARM=%s", token_hex, alarm_str)
+
+        now = datetime.now(timezone.utc)
+        alarm = dateutil.parser.parse(alarm_str)
+
+        if alarm < now:
+            logging.info("Alarm has passed; ignore request")
+            return "<html><body><p>Alarm has passed</p></body></html>"
+
+        interval = alarm - now
+        logging.debug("Starting timer: TOKEN=%s, INTERVAL=%r", token_hex, interval)
+        Timer(
+            interval.seconds,
+            push_notification,
+            args=[token_hex]
+        ).start()
+
+        return "<html><body><p>Alarm set</p></body></html>"
+
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return "<html><body><p>Sorry wrong page</p></body></html>"
+
+
+def push_notification(token_hex):
+    global pusher
+
+    if pusher is None:
+        logging.error("No pusher setup!")
+        raise Exception("No pusher setup!")
+
+    logging.debug("Pushing notification: TOKEN=%s", token_hex)
+    pusher.push(token_hex)
+
+
+class NotificationPusher(object):
+
+    def __init__(self, topic, auth_key_path, auth_key_id, team_id, use_sandbox=False):
+        self.topic = topic
+
+        self.client = APNsClient(
+            TokenCredentials(auth_key_path, auth_key_id, team_id),
+            use_sandbox=use_sandbox,
+            use_alternative_port=False
+        )
+
+    def push(self, token_hex):
+        payload = Payload(alert="Hello World!", sound="default", badge=1)
+        self.client.send_notification(token_hex, payload, self.topic)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Starts Flask server and configures the server.')
+
+    parser.add_argument('--p8', required=True, type=str, help='p8 keystore from Apple')
+    parser.add_argument('--key-id', required=True, type=str, help='auth key id required for Apple push notification')
+    parser.add_argument('--team-id', required=True, type=str, help='auth key id required for Apple push notification')
+
+    args = parser.parse_args()
+
+    logging.info("Init push notifier.")
+    pusher = NotificationPusher(
+        topic="com.chillzie.Chillzie",
+        auth_key_path=args.p8,
+        auth_key_id=args.key_id,
+        team_id=args.team_id,
+        use_sandbox=True
+    )
+
+    app.run(host='0.0.0.0')
